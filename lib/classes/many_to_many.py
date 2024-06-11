b504b8297d24f3a5581d3ad64ca7f9479548b80b
Noah chirchir <chirchirnoah37@gmail.com>
2024-06-11 07:03:26 +0300

    Modifies many_to_many

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,41 +1,137 @@
 class Band:
     def __init__(self, name, hometown):
-        self.name = name
-        self.hometown = hometown
+        self.name = name  
+        if not isinstance(hometown, str):
+            raise Exception("Hometown must be a string")
+        self._hometown = hometown
+        self._concerts = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise Exception("Band name must be a non-empty string")
+        self._name = value
+
+    @property
+    def hometown(self):
+        return self._hometown
+
+    @hometown.setter
+    def hometown(self, value):
+        if hasattr(self, '_hometown'):
+            raise Exception("Hometown cannot be changed")
+        self._hometown = value
 
     def concerts(self):
-        pass
+        return self._concerts if self._concerts else None
 
     def venues(self):
-        pass
+        if not self._concerts:
+            return None
+        return list(set(concert.venue for concert in self._concerts))
 
     def play_in_venue(self, venue, date):
-        pass
+        concert = Concert(date, self, venue)
+        if concert not in self._concerts:
+            self._concerts.append(concert)
+            venue.add_concert(concert)
+        return concert
 
     def all_introductions(self):
-        pass
+        introductions = []
+        for concert in self._concerts:
+            venue_city = concert.venue.city
+            introduction = f"Hello {venue_city}!!!!! We are {self.name} and we're from {self.hometown}"
+            introductions.append(introduction)
+        return introductions
 
 
 class Concert:
+    all = []
+
     def __init__(self, date, band, venue):
         self.date = date
         self.band = band
         self.venue = venue
+        band._concerts.append(self)
+        venue.add_concert(self)
+        Concert.all.append(self)
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise Exception("Date must be a non-empty string")
+        self._date = value
+
+    @property
+    def band(self):
+        return self._band
+
+    @band.setter
+    def band(self, value):
+        if not isinstance(value, Band):
+            raise Exception("Band must be of type Band")
+        self._band = value
+
+    @property
+    def venue(self):
+        return self._venue
+
+    @venue.setter
+    def venue(self, value):
+        if not isinstance(value, Venue):
+            raise Exception("Venue must be of type Venue")
+        self._venue = value
 
     def hometown_show(self):
-        pass
+        return self.band.hometown == self.venue.city
 
     def introduction(self):
-        pass
-
+     return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
 
 class Venue:
     def __init__(self, name, city):
-        self.name = name
+        self.name = name  
         self.city = city
+        self._concerts = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise Exception("Venue must be a non-empty string")
+        self._name = value
+
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise Exception("City must be a non-empty string")
+        self._city = value
 
     def concerts(self):
-        pass
+        return self._concerts if self._concerts else None
 
     def bands(self):
-        pass
+        if not self._concerts:
+            return None
+        return list(set(concert.band for concert in self._concerts))
+
+    def add_concert(self, concert):
+        if not isinstance(concert, Concert):
+            raise Exception("Concert must be of type Concert")
+        self._concerts.append(concert)
--- lib/testing/band_test.py
+++ lib/testing/band_test.py
@@ -1,3 +1,4 @@
+import pytest
 from classes.many_to_many import Band
 from classes.many_to_many import Concert
 from classes.many_to_many import Venue
@@ -23,12 +24,12 @@ class TestBand:
         assert band_1.name == "spicegurls"
 
         # comment out the next two lines if using Exceptions
-        band_1.name = 7
-        assert band_1.name == "spicegurls"
+        #band_1.name = 7
+        #assert band_1.name == "spicegurls"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     band_1.name = 7
+        with pytest.raises(Exception):
+            band_1.name = 7
 
     def test_name_has_length(self):
         """names are longer than 0 characters"""
@@ -36,12 +37,12 @@ class TestBand:
         assert len(band_1.name) > 0
 
         # comment out the next two lines if using Exceptions
-        band_1.name = ""
-        assert band_1.name == "boygenius"
+       # band_1.name = ""
+        #assert band_1.name == "boygenius"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     band_1.name = ""
+        with pytest.raises(Exception):
+            band_1.name = ""
 
     def test_has_hometown(self):
         """Band is instantiated with a hometown"""
@@ -57,21 +58,21 @@ class TestBand:
         assert isinstance(band_1.hometown, str)
 
         # comment out the next three lines if using Exceptions
-        band_1.hometown = "Boston"
-        assert isinstance(band_1.hometown, str)
-        assert band_1.hometown == "NYC"
+        #band_1.hometown = "Boston"
+        #assert isinstance(band_1.hometown, str)
+       # assert band_1.hometown == "NYC"
 
         # comment out the next two lines if using Exceptions
-        band_1.hometown = 7
-        assert band_1.hometown == "NYC"
+        #band_1.hometown = 7
+       # assert band_1.hometown == "NYC"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     band_1.hometown = "Boston"
+        with pytest.raises(Exception):
+            band_1.hometown = "Boston"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     band_1 = Band(name="boygenius", hometown=7)
+        with pytest.raises(Exception):
+             band_1 = Band(name="boygenius", hometown=7)
 
     def test_hometown_has_length(self):
         """hometowns are longer than 0 characters"""
@@ -79,12 +80,12 @@ class TestBand:
         assert len(band_1.hometown) > 0
 
         # comment out the next two lines if using Exceptions
-        band_1.hometown = ""
-        assert band_1.hometown == "NYC"
+        #band_1.hometown = ""
+       # assert band_1.hometown == "NYC"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     band_1.hometown = ""
+        with pytest.raises(Exception):
+             band_1.hometown = ""
 
     def test_concerts(self):
         """band has many concerts"""
